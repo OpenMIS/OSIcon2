a61e70630cb2e44d81db717a3b9e6a464efc5587
OpenMIS <11686738@qq.com>
2022-12-17 12:50:42 +0800

    。

---
+++ Projects/OSIcon2/About.cs
@@ -0,0 +1,22 @@
+#pragma warning disable
+/*
+ * OSIcon
+ * Author: Tiago Concei玢o
+ * 
+ * https://github.com/sn4k3/OSIcon
+ * http://www.codeproject.com/Articles/50064/OSIcon
+ */
+namespace OSIcon;
+
+public static class About
+{
+  /// <summary>
+  /// Library Author
+  /// </summary>
+  public const string ProjectAuthor = "Tiago Concei玢o";
+  /// <summary>
+  /// Library HomePage
+  /// </summary>
+  public const string ProjectUrl = "http://www.codeproject.com/Articles/50064/OSIcon";
+  //public const double ProjectNETFramework = 4.0;
+}
---
+++ Projects/OSIcon2/FolderState.cs
@@ -0,0 +1,24 @@
+/*
+ * OSIcon
+ * Author: Tiago Conceição
+ * 
+ * https://github.com/sn4k3/OSIcon
+ * http://www.codeproject.com/Articles/50064/OSIcon
+ */
+namespace OSIcon;
+
+/// <summary>
+/// Options to specify whether folders should be in the open or closed state.
+/// </summary>
+public enum FolderState : byte
+{
+  /// <summary>
+  /// Specify open folder.
+  /// </summary>
+  Open = 0,
+
+  /// <summary>
+  /// Specify closed folder.
+  /// </summary>
+  Closed = 1
+}
