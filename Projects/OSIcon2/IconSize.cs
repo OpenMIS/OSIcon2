4b1f7562431d84c56d5602fd4bb4b00b6f2a38c9
OpenMIS <11686738@qq.com>
2022-12-21 18:25:47 +0800

    .

--- Projects/OSIcon2/IconSize.cs
+++ Projects/OSIcon2/IconSize.cs
@@ -14,28 +14,30 @@ namespace OSIcon;
 [Flags]
 public enum IconSize : uint
 {
+  /// <summary>
+  /// Specify small icon - 16 pixels by 16 pixels.
+  /// </summary>
+  Small = 1,
 
   /// <summary>
   /// Specify large icon - 32 pixels by 32 pixels.
   /// </summary>
-  Large = 0x0,
-  /// <summary>
-  /// Specify small icon - 16 pixels by 16 pixels.
-  /// </summary>
-  Small = 0x1,
+  Large = 2,
+
   /// <summary>
   /// Specify extra large icon - 48 pixels by 48 pixels.
   /// Only available under XP and latter; other OS return the Large Icon ImageList.
   /// </summary>
-  ExtraLarge = 0x2,
+  ExtraLarge = 4,
 
-  /// <summary>
-  /// These images are the size specified by GetSystemMetrics called with SM_CXSMICON and GetSystemMetrics called with SM_CYSMICON.
-  /// </summary>
-  SysSmall = 0x3,
 
   /// <summary>
   /// Windows Vista and later. The image is normally 256x256 pixels.
   /// </summary>
-  Jumbo = 0x4
+  Jumbo = 8,
+
+  /// <summary>
+  /// These images are the size specified by GetSystemMetrics called with SM_CXSMICON and GetSystemMetrics called with SM_CYSMICON.
+  /// </summary>
+  SysSmall = 16
 }
